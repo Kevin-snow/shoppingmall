39743f0ff2272f889d429703cd8eddd38edd2857
Kevin-snow <beijing_yaya_2011@163.com>
2019-02-28 23:58:10 +0800

    增加保存用户方法

--- src/main/java/cn/kevin/shoppingmall/dao/OrderMapper.java
+++ src/main/java/cn/kevin/shoppingmall/dao/OrderMapper.java
@@ -1,9 +1,11 @@
 package cn.kevin.shoppingmall.dao;
 
+import cn.kevin.shoppingmall.entity.User;
+
 /**
  * Created by Kevin on 2019/2/26.
  */
 public interface OrderMapper {
 
-
+    public void saveUser(User user);
 }
